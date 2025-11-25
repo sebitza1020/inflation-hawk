ddf737d88f1dcff5e33234d68b8d11b6b5e8eb8d
Sebastian Muntean <munteansebastian@gmail.com>
2025-11-25 14:09:33 +0200

    feat: Add production environment configuration

---
+++ inflation-hawk-ui/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCfEIXgVcXOyant4RVT8w2CvDhbc_Sm698",
+    authDomain: "inflation-hawk.firebaseapp.com",
+    projectId: "inflation-hawk",
+    storageBucket: "inflation-hawk.firebasestorage.app",
+    messagingSenderId: "422529876814",
+    appId: "1:422529876814:web:1468348ec3490eb924541f"
+  },
+  apiUrl: 'https://inflation-hawk-api.onrender.com/'
+};
